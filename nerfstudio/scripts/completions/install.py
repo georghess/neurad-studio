e0025bcd8c9a91bf127528e80dddeb34f16dd846
Carl Lindström <carlinds@student.chalmers.se>
2024-04-24 10:14:40 +0200

    Update distribution name in install script

--- nerfstudio/scripts/completions/install.py
+++ nerfstudio/scripts/completions/install.py
@@ -45,7 +45,7 @@ HEADER_LINE = "# Source nerfstudio autocompletions."
 
 def _get_all_entry_points() -> List[str]:
     # TODO: we should filter out entrypoints that are not tyro CLIs.
-    entry_points = importlib_metadata.distribution("nerfstudio").entry_points
+    entry_points = importlib_metadata.distribution("neurad-studio").entry_points
     return [x.name for x in entry_points]
 
 
